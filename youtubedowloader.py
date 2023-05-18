3bee29e94131d17fb18c551864057ec959c65bb4
awessakhtar <awessakhtar@gmail.com>
2023-05-18 20:22:46 +0500

    bug cleared now video is downloading it was a missing paranthesis in function download()

--- youtubedowloader.py
+++ youtubedowloader.py
@@ -4,11 +4,11 @@ def downlaod(link):
     youtubeObject = YouTube(link)
     youtubeObject =youtubeObject.streams.get_highest_resolution()
     try:
-        youtubeObject.download
+        youtubeObject.download()
 
     except:
         print("An error has occurred!,\n please try again")
-        print("Download completed suscessfully")
+    print("Download completed suscessfully")
 
 
 link = input("Enter URL for youtube video: ")
