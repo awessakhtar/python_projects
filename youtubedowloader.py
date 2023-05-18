f229e7746a00f71cc70e84b097b1d0cb3b5dc9f4
awessakhtar <awessakhtar@gmail.com>
2023-05-18 20:14:23 +0500

    basic youtube downloader added. but it is not downloading

---
+++ youtubedowloader.py
@@ -0,0 +1,15 @@
+from pytube import YouTube
+
+def downlaod(link):
+    youtubeObject = YouTube(link)
+    youtubeObject =youtubeObject.streams.get_highest_resolution()
+    try:
+        youtubeObject.download
+
+    except:
+        print("An error has occurred!,\n please try again")
+        print("Download completed suscessfully")
+
+
+link = input("Enter URL for youtube video: ")
+downlaod(link)
